016211389e8be03430d79eb6a1dc9d5fcd33b630
monthem <lpoeh01@gmail.com>
2020-07-26 12:39:17 +0900

    INIT: initialized expected components and screens

---
+++ components/RankViewer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const RankViewer = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default RankViewer

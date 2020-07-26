e4f748c6bc3968a832ba4082fcf7630dcc68e182
monthem <lpoeh01@gmail.com>
2020-07-26 12:38:48 +0900

    Initialized native project

---
+++ App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import 'react-native-gesture-handler';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App: () => React.ReactNode = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;

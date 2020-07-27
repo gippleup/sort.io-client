1aa207fc90a3fd12c78ac3a9a48e4ba42aa71af0
monthem <lpoeh01@gmail.com>
2020-07-27 22:52:37 +0900

    MISC: added EndGameInfo(blank)

---
+++ components/EndGameInfo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const EndGameInfo = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default EndGameInfo;

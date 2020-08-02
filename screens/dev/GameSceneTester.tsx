c501f0ef9cf5bdf73f6b4c08bdcd85f58fcbe378
monthem <lpoeh01@gmail.com>
2020-08-02 13:43:58 +0900

    MISC: added map prop to GameScene component

--- screens/dev/GameSceneTester.tsx
+++ screens/dev/GameSceneTester.tsx
@@ -2,8 +2,26 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import GameScene from '../../components/GameScene'
 
+const exStackMap: BlockTypes[][] = [
+  [0, 0, 0],
+  [1, 1],
+  [-1, -1, -1, -1, -1],
+  [-1, -1, -1, -1],
+  [-1, -1, -1, -1],
+  [2, 2, 3, 3],
+  [3, 4, 4, -1, -1],
+];
+
 const GameSceneTester = () => {
-  return <GameScene title={'하드 1-5'} timeLimit={60} maxScore={5} />;
+  return (
+    <GameScene
+      skin="spiky"
+      map={exStackMap}
+      title={'하드 1-5'}
+      timeLimit={60}
+      maxScore={5}
+    />
+  );
 };
 
 export default GameSceneTester;

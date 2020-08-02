f51421d13552128444d1dfddf0b8f707dc90b5a3
monthem <lpoeh01@gmail.com>
2020-08-02 13:43:19 +0900

    MODIFY: modified scorechecker as class component and replace prop-based render with ref-based render through .setScore method

--- components/ScoreChecker.tsx
+++ components/ScoreChecker.tsx
@@ -1,9 +1,11 @@
-import React, { Fragment } from 'react';
-import {View, Text} from 'react-native';
+import React, {Fragment, RefObject} from 'react';
+import {View, Text, Easing} from 'react-native';
 import {skins} from './BlockStack/skinMap';
 import styled from 'styled-components';
 import BlockFrame from './BlockStack/BlockFrame';
 import ScoreIcon from './ScoreChecker/ScoreIcon';
+import Constants from '../assets/Constants';
+import RefBox from './RefBox';
 
 const ScoreCheckerContainer: typeof View = styled(View)``;
 
@@ -13,7 +15,7 @@ const Row: typeof View = styled(View)`
 
 type ScoreCheckerProps = {
   maxScore: number;
-  curScore: number;
+  initialScore: number;
   skin: skins;
   layout: number[][];
   scale: number;
@@ -21,61 +23,106 @@ type ScoreCheckerProps = {
   intervalHeight?: number;
 };
 
-const ScoreChecker: React.FC<ScoreCheckerProps> = (props) => {
-  let iconToDrawCount = props.maxScore;
-  let scoreToCheckCount = props.curScore;
+class ScoreChecker extends React.Component<ScoreCheckerProps> {
+  constructor(props: ScoreCheckerProps) {
+    super(props);
+    this.setScore = this.setScore.bind(this);
+  }
 
-  const Space = () => (
-    <View
-      style={{
-        width: props.intervalWidth ? props.intervalWidth : 15 * props.scale,
-        height: props.intervalHeight ? props.intervalHeight : 15 * props.scale,
-      }}
-    />
-  );
+  icons: RefObject<RefBox>[] = [];
 
-  const InvisibleFrame = () => (
-    // eslint-disable-next-line react-native/no-inline-styles
-    <View style={{opacity: 0}}>
-      <BlockFrame pieceCount={1} scale={props.scale} />
-    </View>
-  );
+  setScore(score: number) {
+    this.icons.forEach((iconRef, i) => {
+      const targetRef = iconRef.current;
+      if (!targetRef) {
+        return;
+      }
+      if (i < score && targetRef?.opacity < 1) {
+        targetRef.setOpacity(1);
+        targetRef.setScale(0);
+        targetRef.animateScale(1, 300, Easing.in(Easing.elastic(3))).start();
+      } else if (i >= score && targetRef?.opacity > 0) {
+        targetRef.animateScale(0, 300, Easing.in(Easing.back(3))).start();
+        targetRef.animateOpacity(0).start();
+      }
+    });
+  }
 
-  const mappedLayout = props.layout.map((row, i) => {
-    const isEndOfRow = i === props.layout.length - 1;
-    return (
-      <Fragment key={'fragment' + i}>
-        <Row key={'row' + i}>
-          {row.map((bool, j) => {
-            if (!iconToDrawCount) {
-              return;
-            } else if (bool) {
-              const iconType = scoreToCheckCount > 0 ? 0 : 9;
-              const isEndOfCol = j === row.length - 1;
-              scoreToCheckCount--;
-              iconToDrawCount--;
-              return (
-                <Fragment key={'fragment' + i + j}>
-                  <ScoreIcon
-                    key={'scoreIcon' + j}
-                    scale={props.scale}
-                    skin={props.skin}
-                    type={iconType}
-                  />
-                  {isEndOfCol ? <></> : <Space key={'space' + i + j} />}
-                </Fragment>
-              );
-            } else {
-              return <InvisibleFrame key={'invisibleFrame' + j} />;
-            }
-          })}
-        </Row>
-        {isEndOfRow ? <></> : <Space key={'space' + i} />}
-      </Fragment>
+  componentDidMount() {
+    this.setScore(this.props.initialScore);
+  }
+
+  render() {
+    const {props, state} = this;
+    let iconToDrawCount = this.props.maxScore;
+    // let scoreToCheckCount = state.curScore;
+    const Space = () => (
+      <View
+        style={{
+          width: props.intervalWidth ? props.intervalWidth : 15 * props.scale,
+          height: props.intervalHeight
+            ? props.intervalHeight
+            : 15 * props.scale,
+        }}
+      />
     );
-  });
 
-  return <ScoreCheckerContainer>{mappedLayout}</ScoreCheckerContainer>;
-};
+    const InvisibleFrame = () => (
+      // eslint-disable-next-line react-native/no-inline-styles
+      <View style={{opacity: 0}}>
+        <BlockFrame pieceCount={1} scale={props.scale} />
+      </View>
+    );
+
+    const mappedLayout = props.layout.map((row, i) => {
+      const isEndOfRow = i === props.layout.length - 1;
+      return (
+        <Fragment key={'fragment' + i}>
+          <Row key={'row' + i}>
+            {row.map((bool, j) => {
+              if (!iconToDrawCount) {
+                return;
+              } else if (bool) {
+                // const iconType = scoreToCheckCount > 0 ? 0 : 9;
+                const isEndOfCol = j === row.length - 1;
+                // scoreToCheckCount--;
+                iconToDrawCount--;
+                const iconRef = React.createRef<RefBox>();
+                this.icons.push(iconRef);
+                return (
+                  <Fragment key={'fragment' + i + j}>
+                    <View
+                      style={{
+                        width: Constants.blockWidth * props.scale,
+                        height: Constants.blockHeight.count(1) * props.scale,
+                      }}>
+                      <View style={{position: 'absolute'}}>
+                        <BlockFrame pieceCount={1} scale={props.scale} />
+                      </View>
+                      <RefBox style={{opacity: 0}} ref={iconRef}>
+                        <ScoreIcon
+                          key={'scoreIcon' + j}
+                          scale={props.scale}
+                          skin={props.skin}
+                          type={0}
+                        />
+                      </RefBox>
+                    </View>
+                    {isEndOfCol ? <></> : <Space key={'space' + i + j} />}
+                  </Fragment>
+                );
+              } else {
+                return <InvisibleFrame key={'invisibleFrame' + j} />;
+              }
+            })}
+          </Row>
+          {isEndOfRow ? <></> : <Space key={'space' + i} />}
+        </Fragment>
+      );
+    });
+
+    return <ScoreCheckerContainer>{mappedLayout}</ScoreCheckerContainer>;
+  }
+}
 
 export default ScoreChecker;

56aea03278198ebc83e0782f13c2ff280e85f391
monthem <lpoeh01@gmail.com>
2020-08-13 23:38:38 +0900

    ADD: added map generating api, merged country flag api to one file

---
+++ api/sortio.ts
@@ -0,0 +1,28 @@
+import { BlockTypes } from "../components/Block/Types"
+
+const API_BASE = 'http://ec2-3-35-52-143.ap-northeast-2.compute.amazonaws.com:3000'
+
+export const getCountryIconSvg = (lat: number, lng: number) => {
+  const url = `${API_BASE}/country/icon`
+  return fetch(`${url}?lat=${lat}&lng=${lng}`)
+  .then((res)=> res.text())
+}
+
+
+export type MapOption = {
+  blockStackCount: number;
+  stackLengthMax: number;
+  stackLengthMin: number;
+  maxScore: number;
+  colorCount: number;
+  shuffleCount?: number;
+}
+
+
+export const generateMap = (option: MapOption): Promise<{answer: BlockTypes[][], question: BlockTypes[][]}> => {
+  const mappedQuery = Object.entries(option).map(([key, value]) => {
+    return `${key}=${value}`;
+  }).join('&');
+  const url = `${API_BASE}/map/generate?${mappedQuery}`
+  return fetch(url).then((res) => res.json())
+}
--- components/CounryFlagIcon/util.ts
+++
@@ -1,6 +0,0 @@
-import * as RNSvgTransformer from 'react-native-svg-transformer';
-export const getCountryIconSvg = (lat: number, lng: number) => {
-  const baseurl = 'http://ec2-3-35-52-143.ap-northeast-2.compute.amazonaws.com:3000/country/icon'
-  return fetch(`${baseurl}?lat=${lat}&lng=${lng}`)
-  .then((res)=> res.text())
-}

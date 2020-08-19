bedd238c657dbf3ae8458c432eff2eb9dc7c3867
monthem <lpoeh01@gmail.com>
2020-08-19 23:03:19 +0900

    ADD: added NotoSans Component which uses NotoSansKR as default

--- components/Generic/StyledComponents.tsx
+++ components/Generic/StyledComponents.tsx
@@ -1,4 +1,4 @@
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 import styled from 'styled-components';
 
 export const FlexHorizontal: typeof View = styled(View)`
@@ -20,3 +20,49 @@ Space.defaultProps = {
   width: 0,
   height: 0,
 };
+
+type NotoSansProps = {
+  type: 'Black' | 'Bold' | 'Light' | 'Medium' | 'Regular' | 'Thin';
+  size?: number;
+}
+
+export const NotoSans: typeof Text & React.ComponentClass<NotoSansProps, {}> = styled(Text)<NotoSansProps>`
+  font-family: NotoSansKR-${(props) => props.type};
+  line-height: ${(props) => props.size * 9/6}px;
+  font-size: ${(props) => props.size}px;
+`;
+
+NotoSans.defaultProps = {
+  type: 'Regular',
+  size: 14,
+}
+
+export const TitleText: typeof Text = styled(Text)`
+  font-family: NotoSansKR-Black;
+  font-size: 25px;
+  line-height: 33px;
+`;
+
+export const SubTitleText: typeof Text = styled(Text)`
+  font-family: NotoSansKR-Bold;
+  font-size: 18px;
+  line-height: 23px;
+`;
+
+export const ContentText: typeof Text = styled(Text)`
+  font-family: NotoSansKR-Regular;
+  font-size: 14px;
+  line-height: 20px;
+`;
+
+export const SmallText: typeof Text = styled(Text)`
+  font-family: NotoSansKR-Medium;
+  font-size: 10px;
+  line-height: 16px;
+`;
+
+export const ExtraSmallText: typeof Text = styled(Text)`
+  font-family: NotoSansKR-Light;
+  font-size: 7px;
+  line-height: 12px;
+`;

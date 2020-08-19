5fc2506bec3aeb68f1d547f73e0b14d0fdc5915e
monthem <lpoeh01@gmail.com>
2020-08-19 23:01:41 +0900

    MODIFY: modified spiky skin colorthme to be more distinguishable

--- components/Block/Spiky/ColorTheme.ts
+++ components/Block/Spiky/ColorTheme.ts
@@ -1,3 +1,5 @@
+import chroma from 'chroma-js';
+
 type SpikeSkinColorTheme = {
   cap: string;
   spike: string;
@@ -6,49 +8,76 @@ type SpikeSkinColorTheme = {
   feet: string;
 };
 
-const colorTheme: {[index: number]: SpikeSkinColorTheme} = {
-  0: {
-    cap: 'red',
-    spike: 'lightgrey',
-    bodyFill: 'white',
-    bottomFill: '#FF8E8E',
-    feet: '#BC0000',
-  },
-  1: {
-    feet: '#4FB5FF',
-    bodyFill: '#FFB545',
-    bottomFill: '#83FF8F',
-    cap: '#E3FF3A',
-    spike: '#8A8A8A',
-  },
-  2: {
-    feet: '#FF60A3',
-    bodyFill: '#FFE7C3',
-    bottomFill: '#9F64FF',
-    cap: '#3A992B',
-    spike: '#8A8A8A',
-  },
-  3: {
-    feet: '#A893FF',
-    bodyFill: '#808080',
-    bottomFill: 'dodgerblue',
-    cap: '#F2BDFF',
-    spike: '#E7E7E7',
-  },
-  4: {
-    feet: '#FFF9BD',
-    bodyFill: '#A8ABFF',
-    bottomFill: '#5B5B5B',
-    cap: '#FFFFFF',
-    spike: '#FFFFFF',
-  },
-  50: {
-    feet: 'grey',
-    bodyFill: 'darkgrey',
-    bottomFill: 'grey',
-    cap: 'grey',
-    spike: 'grey',
-  },
+
+const randomIndexFromArray = (array: any[]): number => {
+  const randomValidIndex = Math.floor(Math.random() * array.length);
+  return randomValidIndex;
+}
+
+const pickRandomFromArray = (array: any[]) => {
+  return array[randomIndexFromArray(array)];
+}
+
+const extractRandomFromArray = (array: any[]) => {
+  const randomIndex = randomIndexFromArray(array);
+  const pickedEle = array[randomIndex];
+  const front = array.slice(0, randomIndex);
+  const latter = array.slice(randomIndex + 1, array.length);
+  const extractedArray = front.concat(latter)
+  return {
+    pickedEle,
+    extractedArray,
+  };
+}
+
+const mainColor = chroma('red').hex();
+
+const colorArr = [];
+const blockTypeCount = 24;
+const hue = chroma(mainColor).get('hsl.h');
+for (let i = 0; i < blockTypeCount; i += 1) {
+  const adjustedColor = chroma(mainColor)
+    .set('hsl.h', hue + 360 / blockTypeCount * i)
+    .set('hsl.l',
+      i % 4 === 0 ? 0.7
+        : i % 4 === 1 ? 0.6
+          : i % 4 === 2 ? 0.4
+            : i % 4 === 3 ? 0.4 : '')
+    .set('hsl.s',
+      i % 4 === 0 ? 1
+        : i % 4 === 1 ? 0.6
+          : i % 4 === 2 ? 0.6
+            : i % 4 === 3 ? 1 : '')
+    .hex()
+  colorArr.push(adjustedColor)
+}
+
+let index = colorArr.map((_, i) => i);
+
+const colorTheme = colorArr.reduce((
+  acc: {[index: number]: SpikeSkinColorTheme},
+  color: string,
+  i: number
+) => {
+  const randomIndex = extractRandomFromArray(index);
+  index = randomIndex.extractedArray;
+  acc[randomIndex.pickedEle] = {
+    cap: chroma(color).set('hsl.l', 0.8).hex(),
+    spike: chroma(color).hex(),
+    bodyFill: chroma(color).hex(),
+    bottomFill: chroma(color).set('hsl.l', Math.max(chroma(color).get('hsl.l') - 0.3, 0.3)).hex(),
+    feet: chroma(color).set('hsl.l', 0.8).hex(),
+  };
+  return acc;
+}, {})
+
+colorTheme[50] = {
+  cap: 'grey',
+  spike: 'grey',
+  bodyFill: 'darkgrey',
+  bottomFill: 'grey',
+  feet: 'grey',
 };
+
 
 export default colorTheme;

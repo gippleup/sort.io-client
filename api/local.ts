f436819eabe49c022bc284ab49aa5807f19e4e82
monthem <lpoeh01@gmail.com>
2020-08-19 23:11:45 +0900

    ADD: added local storage api for handling gold and ticket change

--- api/local.ts
+++ api/local.ts
@@ -9,6 +9,7 @@ export type SortIoUser = {
   name: string;
   isTemp: boolean;
   gold: number;
+  ticket: number;
 }
 
 export type SinglePlayData = {
@@ -32,10 +33,12 @@ export type MultiPlayData = {
 }
 
 export type PlayData = {
-  user: SortIoUser | null;
+  user: SortIoUser;
   single: SinglePlayData[];
   multi: MultiPlayData[];
 }
+
+const subscriber: Function[] = [];
 
 const encrypt = (str: string) => {
   const encrypted = CryptoJS.AES.encrypt(str, publicKey).toString();
@@ -47,15 +50,18 @@ const decrypt = (str: string) => {
   return bytes.toString(CryptoJS.enc.Utf8);
 }
 
-export const getPlayData = () => {
+export const getPlayData = (): Promise<PlayData> => {
   return new Promise ((resolve, reject) => {
     AsyncStorage.getItem('playData')
       .then((data) => {
         if (data) {
-          resolve(JSON.parse(decrypt(data)));
+          const decryptedData = JSON.parse(decrypt(data));
+          resolve(decryptedData);
+          subscriber.forEach((callback) => callback(decryptedData))
         } else {
           initializePlayData().then((data) => {
             resolve(data);
+            subscriber.forEach((callback) => callback(data));
           });
         }
       })
@@ -63,26 +69,67 @@ export const getPlayData = () => {
 }
 
 export const setPlayData = (data: PlayData) => {
+  subscriber.forEach((callback) => callback(data))
   const encrypted = encrypt(JSON.stringify(data));
   return AsyncStorage.setItem('playData', encrypted);
 }
 
-export const initializePlayData = async () => {
+export const initializePlayData = async (): Promise<PlayData> => {
   const serverGeneratedGuest = await makeGuestId();
   const tempOfflineUser = {
     id: null,
     gold: 0,
     isTemp: true,
     name: 'tempOfflineLogin',
+    ticket: 0,
   };
   
   const data: PlayData = {
-    user: null,
+    user: serverGeneratedGuest || tempOfflineUser,
     single: [],
     multi: [],
   }
   
-  data.user = serverGeneratedGuest || tempOfflineUser;
   setPlayData(data);
   return data;
+}
+
+export const modifyGold = (amount: number) => {
+  return getPlayData().then((data) => {
+    const curGold = data.user.gold;
+    setPlayData({
+      ...data,
+      user: {
+        ...data.user,
+        gold: curGold + amount,
+      }
+    })
+  })
+}
+
+export const modifyTicket = (amount: number) => {
+  return getPlayData().then((data)=> {
+    const curTicket = data.user.ticket;
+    setPlayData({
+      ...data,
+      user: {
+        ...data.user,
+        ticket: curTicket + amount,
+      }
+    })
+  })
+}
+
+export const useGold = (amount: number) => modifyGold(-amount);
+export const saveGold = (amount: number) => modifyGold(amount);
+
+export const useTicket = (amount: number) => modifyTicket(-amount);
+export const saveTicket = (amount: number) => modifyTicket(amount);
+
+export const clearPlayData = () => {
+  return AsyncStorage.removeItem('playData');
+}
+
+export const subscribePlayData = (callback: (data: PlayData) => any) => {
+  subscriber.push(callback);
 }

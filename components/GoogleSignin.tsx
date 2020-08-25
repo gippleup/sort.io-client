53bae148fee0e46b55e29da4e431f3e7df6bbf6f
monthem <lpoeh01@gmail.com>
2020-08-25 21:45:42 +0900

    ADD: added prop.active to GoogleIcon, added props.onSignOut to GoogleSignin

--- components/GoogleSignin.tsx
+++ components/GoogleSignin.tsx
@@ -1,34 +1,73 @@
 import React, { Component } from 'react'
-import {GoogleSignin as GoogleSigninModule, statusCodes} from '@react-native-community/google-signin'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import {GoogleSignin as GoogleSigninModule, statusCodes, User} from '@react-native-community/google-signin'
 import GoogleIcon from './GoogleSignIn/GoogleIcon'
+import { NotoSans, FlexHorizontal, Space } from './Generic/StyledComponents'
+import { View, Modal, TouchableOpacity } from 'react-native'
+import styled from 'styled-components'
+
+const PopupContainer = styled(View)`
+  flex: 1;
+  background-color: rgba(0,0,0,0.5);
+  justify-content: center;
+`;
+
+const PopupMessageContainer = styled(View)`
+  padding: 25px;
+  align-self: center;
+  background-color: white;
+  border-radius: 20px;
+  elevation: 10;
+`;
+
+const PopupButton: typeof View | React.FC<{tomato?: boolean}> = styled(View)<{tomato: boolean}>`
+  padding: 15px;
+  background-color: ${(props) => props.tomato ? 'tomato' : 'lightgrey'};
+  border-radius: 15px;
+  width: 100px;
+  align-items: center;
+`;
 
 GoogleSigninModule.configure({
   webClientId: '139458839925-bqm86mnndgcd2d3e47b5i3ljplovff57.apps.googleusercontent.com',
   forceCodeForRefreshToken: true,
 })
 
+type GoogleSignInProps = {
+  onSignIn?: (user: User) => any;
+  onSignOut?: () => any;
+  size?: number;
+}
+
 type GoogleSignInState = {
   userInfo: null | any;
   isSignedIn: boolean;
+  modalVisible: boolean;
 }
 
-export class GoogleSignin extends Component<any, GoogleSignInState> {
+export class GoogleSignin extends Component<GoogleSignInProps, GoogleSignInState> {
   constructor(props: any) {
     super(props)
     this.state = {
       userInfo: null,
       isSignedIn: false,
+      modalVisible: false,
     }
     this._checkIfSignedIn = this._checkIfSignedIn.bind(this);
     this._signInIfNotSignedIn = this._signInIfNotSignedIn.bind(this);
+    this._checkIfSignedIn((signedIn) => {
+      this.setState({
+        ...this.state,
+        isSignedIn: signedIn,
+      })
+    })
   }
   _signIn = async () => {
+    const {props} = this;
     try {
       await GoogleSigninModule.hasPlayServices()
       const userInfo = await GoogleSigninModule.signIn()
-      this.setState({userInfo, isSignedIn: true})
-      console.log(userInfo);
+      this.setState({...this.state, userInfo, isSignedIn: true})
+      if (props.onSignIn) props.onSignIn(userInfo);
     } catch (error) {
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
         // user cancelled the login flow
@@ -41,19 +80,13 @@ export class GoogleSignin extends Component<any, GoogleSignInState> {
       }
     }
   }
-  componentDidMount() {
-    this._getSignedInUserInfo();
-    this._signInIfNotSignedIn();
-  }
   componentDidUpdate() {
   }
-  _checkIfSignedIn = async (callback: (signedIn: boolean) => void) => {
+  _checkIfSignedIn = async (callback?: (signedIn: boolean) => void) => {
     const isSignedIn = await GoogleSigninModule.isSignedIn();
-    callback(isSignedIn);
-  }
-  _getSignedInUserInfo = async () => {
-    const userInfo = await GoogleSigninModule.getCurrentUser();
-    return userInfo;
+    if (callback) {
+      callback(isSignedIn);
+    }
   }
   _signInIfNotSignedIn() {
     this._checkIfSignedIn((signedIn) => {
@@ -66,16 +99,56 @@ export class GoogleSignin extends Component<any, GoogleSignInState> {
     try {
       await GoogleSigninModule.hasPlayServices();
       await GoogleSigninModule.signOut();
-      this.setState({isSignedIn: false})
+      this.setState({isSignedIn: false, userInfo: null, modalVisible: false})
+      if (this.props.onSignOut) this.props.onSignOut();
     } catch (error) {
       console.log(error);
     }
   }
   render() {
+    const {state, props} = this;
+    const setModalVisible = (bool: boolean) => {
+      this.setState({
+        ...state,
+        modalVisible: bool,
+      })
+    }
+    const onPress = () => {
+      if (!state.isSignedIn) {
+        this._signIn();
+      } else {
+        setModalVisible(true);
+      }
+    }
     return (
-      <TouchableOpacity onPress={this.state.isSignedIn ? this._signOut : this._signIn}>
-        <GoogleIcon/>
-      </TouchableOpacity>
+      <>
+        <TouchableOpacity style={{alignSelf: 'center'}} onPress={onPress}>
+          <View style={{alignItems: 'center', justifyContent: 'center', alignSelf: 'center'}}>
+            <GoogleIcon size={props.size} active={state.isSignedIn} />
+          </View>
+        </TouchableOpacity>
+        <Modal transparent visible={state.modalVisible}>
+          <PopupContainer>
+            <PopupMessageContainer>
+              <NotoSans size={20} type="Black">로그아웃 하시겠습니까?</NotoSans>
+            </PopupMessageContainer>
+            <Space height={10} />
+            <FlexHorizontal style={{justifyContent: 'center'}}>
+              <TouchableOpacity onPress={this._signOut}>
+                <PopupButton tomato>
+                  <NotoSans size={16} style={{ color: 'white' }} type="Black">예</NotoSans>
+                </PopupButton>
+              </TouchableOpacity>
+              <Space width={10} />
+              <TouchableOpacity onPress={() => setModalVisible(false)}>
+                <PopupButton>
+                  <NotoSans size={16} type="Black">아니요</NotoSans>
+                </PopupButton>
+              </TouchableOpacity>
+            </FlexHorizontal>
+          </PopupContainer>
+        </Modal>
+      </>
     )
   }
 }

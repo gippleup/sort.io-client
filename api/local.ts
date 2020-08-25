c7d38a3fd2d0a2a7d20f6eddf087a423f72c60c0
monthem <lpoeh01@gmail.com>
2020-08-25 21:43:16 +0900

    DEL: deleted most apis in the local.ts for simplifying data flow

--- api/local.ts
+++ api/local.ts
@@ -1,6 +1,5 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import CryptoJS from 'react-native-crypto-js'
-import { makeGuestId } from './sortio';
 
 const publicKey = "GOTCHA";
 
@@ -10,18 +9,19 @@ export type SortIoUser = {
   isTemp: boolean;
   gold: number;
   ticket: number;
-  itmes: string;
+  items: string;
+  googleId: number | undefined;
 }
 
 export type SinglePlayData = {
-  id?: number;
+  id: null | number;
   userId: null | number; //로컬로 플레이해서 userId가 없는 경우는 추후에 연결됐을 때 싹 필터링해서 업데이트하자.
   createdAt: string;
   difficulty: number;
 }
 
 export type MultiPlayData = {
-  id: number;
+  id: null | number;
   user1: number;
   user2: number;
   winner: number;
@@ -37,8 +37,6 @@ export type PlayData = {
   multi: MultiPlayData[];
 }
 
-const subscriber: {[index: string]: Function} = {};
-
 const encrypt = (str: string) => {
   const encrypted = CryptoJS.AES.encrypt(str, publicKey).toString();
   return encrypted;
@@ -49,102 +47,25 @@ const decrypt = (str: string) => {
   return bytes.toString(CryptoJS.enc.Utf8);
 }
 
-export const getPlayData = (): Promise<PlayData> => {
+export const getLocalPlayData = (): Promise<PlayData> => {
   return new Promise ((resolve, reject) => {
     AsyncStorage.getItem('playData')
       .then((data) => {
         if (data) {
           const decryptedData = JSON.parse(decrypt(data)) as PlayData;
           resolve(decryptedData);
-          Object.values(subscriber).forEach((callback) => callback(decryptedData))
         } else {
-          initializePlayData().then((data) => {
-            resolve(data);
-            Object.values(subscriber).forEach((callback) => callback(data));
-          });
+          resolve(undefined);
         }
+      })
+      .catch((err) => {
+        reject(err)
       })
   })
 }
 
-export const setPlayData = (data: PlayData) => {
-  Object.values(subscriber).forEach((callback) => {
-    if (typeof callback === 'function') {
-      callback(data);
-    }
-  })
+
+export const setLocalPlayData = (data: PlayData) => {
   const encrypted = encrypt(JSON.stringify(data));
   return AsyncStorage.setItem('playData', encrypted);
-}
-
-export const initializePlayData = async (): Promise<PlayData> => {
-  const serverGeneratedGuest = await makeGuestId();
-  const tempOfflineUser = {
-    createdAt: new Date(Date.now()).toISOString(),
-    gold: 0,
-    id: null,
-    isTemp: true,
-    items: "",
-    name: 'tempOfflineLogin',
-    ticket: 0,
-  };
-  
-  const data: PlayData = {
-    user: serverGeneratedGuest || tempOfflineUser,
-    single: [],
-    multi: [],
-  }
-  
-  setPlayData(data);
-  return data;
-}
-
-export const modifyGold = (amount: number) => {
-  return getPlayData().then((data) => {
-    const curGold = data.user.gold;
-    return setPlayData({
-      ...data,
-      user: {
-        ...data.user,
-        gold: curGold + amount,
-      }
-    })
-  })
-}
-
-export const modifyTicket = (amount: number) => {
-  return getPlayData().then((data)=> {
-    const curTicket = data.user.ticket;
-    return setPlayData({
-      ...data,
-      user: {
-        ...data.user,
-        ticket: curTicket + amount,
-      }
-    })
-  })
-}
-
-export const useGold = (amount: number) => modifyGold(-amount);
-export const saveGold = (amount: number) => modifyGold(amount);
-
-export const useTicket = (amount: number) => modifyTicket(-amount);
-export const saveTicket = (amount: number) => modifyTicket(amount);
-
-export const clearPlayData = () => {
-  return AsyncStorage.removeItem('playData');
-}
-
-export const subscribePlayData = (key: string, callback: (data: PlayData) => any) => {
-  subscriber[key] = callback;
-  return () => delete subscriber[key];
-}
-
-export const saveSingleGameResult = (gameResult: SinglePlayData) => {
-  return getPlayData().then((data: PlayData) => {
-    return setPlayData({
-      ...data,
-      single: [...data.single, {...gameResult, id: data.single.length}]
-    })
-  })
 }

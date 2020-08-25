fd0d5333f9c3e0714bddf6a58972e241a05c5c01
monthem <lpoeh01@gmail.com>
2020-08-25 21:46:25 +0900

    MISC: simplified usePlayData hook, just as a minifier

--- hooks/usePlayData.ts
+++ hooks/usePlayData.ts
@@ -1,20 +1,9 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../redux/reducers';
-import { loadPlayData, updatePlayData } from '../redux/actions/playData/creator';
-import { subscribePlayData } from '../api/local';
 
 const usePlayData = () => {
-  const dispatch = useDispatch();
   const playData = useSelector((state: RootState) => state.playData)
-  React.useEffect(() => {
-    if (!playData.loaded) {
-      dispatch(loadPlayData());
-    }
-    subscribePlayData('updatePlayData', (data) => {
-      dispatch(updatePlayData(data));
-    })
-  })
   return playData;
 }
 

0e4b1c22858f9c2f6ec13e907d933cce58f4bc49
monthem <lpoeh01@gmail.com>
2020-08-28 21:28:59 +0900

    MODIFY: modified CancelGamePopup Style to adapt to navigation.screen

--- screens/production/GameScreen/CancelGamePopup.tsx
+++ screens/production/GameScreen/CancelGamePopup.tsx
@@ -2,46 +2,59 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import BasicPopup from '../../../components/Generic/BasicPopup'
 import { NotoSans } from '../../../components/Generic/StyledComponents'
-import usePopup from '../../../hooks/usePopup'
+import { useNavigation, RouteProp, CommonActions } from '@react-navigation/native'
+import { StackNavigationProp } from '@react-navigation/stack'
+import routes, { RootStackParamList } from '../../../router/routes'
 
+type CancelGameNavigationProp = StackNavigationProp<RootStackParamList, 'Popup_CancelGame'>
+type CancelGameRouteProp = RouteProp<RootStackParamList, 'Popup_CancelGame'>
 
-type CancelGamePopupProps = {
-  onPressYes: () => void;
+export type CancelGameParams = {
   text: string;
 }
 
-const CancelGamePopup: React.FC<CancelGamePopupProps> = (props) => {
-  const {text, onPressYes} = props;
-  const popup = usePopup();
+type CancelGameProps = {
+  route: CancelGameRouteProp;
+  navigation: CancelGameNavigationProp;
+}
+
+const CancelGamePopup = (props: CancelGameProps) => {
+  const {text} = props.route.params;
+  const navigation = props.navigation;
   return (
-    <BasicPopup
-      title="PAUSE"
-      buttonAlign="horizontal"
-      buttons={[
-        {
-          text: "예",
-          onPress: () => {
-            popup.hide();
-            if (onPressYes) {
-              onPressYes();
+    <View style={{flex: 1}}>
+      <BasicPopup
+        title="PAUSE"
+        buttonAlign="horizontal"
+        buttons={[
+          {
+            text: "예",
+            onPress: () => {
+              navigation.dispatch((state) => {
+                const filteredRoutes = state.routes.slice(0, state.routes.length - 2);
+                const result = CommonActions.reset({
+                  ...state,
+                  routes: filteredRoutes,
+                  index: filteredRoutes.length - 1,
+                });
+                return result;
+              })
+            },
+            style: {
+              backgroundColor: 'pink',
             }
           },
-          style: {
-            backgroundColor: 'pink',
-          }
-        },
-        {
-          text: "아니요",
-          onPress: () => {
-            popup.hide();
+          {
+            text: "아니요",
+            onPress: navigation.goBack,
+            style: {
+              backgroundColor: 'lightgrey',
+            }
           },
-          style: {
-            backgroundColor: 'lightgrey',
-          }
-        },
-      ]}>
-      <NotoSans type="Black">{text}</NotoSans>
-    </BasicPopup>
+        ]}>
+        <NotoSans type="Black">{text}</NotoSans>
+      </BasicPopup>
+    </View>
   )
 }
 

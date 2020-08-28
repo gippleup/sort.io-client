884536136e38fa3ef3b59487fc22b846653d67e9
monthem <lpoeh01@gmail.com>
2020-08-28 21:27:26 +0900

    MODIFY: modified initial gold and ticket from {gold: 0, ticket:0} to {gold: 500, ticket: 3}, added reducer logic for updateGold, updateTicket, updateSinglePlay, updateMultiPlay

--- redux/reducers/playData.ts
+++ redux/reducers/playData.ts
@@ -1,16 +1,26 @@
-import { PlayData, SortIoUser, setLocalPlayData } from "../../api/local";
+import {
+  PlayData,
+  setLocalPlayData,
+} from "../../api/local";
 import { PlayDataActions } from "../actions/playData/creator";
-import { UPDATE_PLAYDATA, UPDATE_USER } from "../actions/playData/types";
+import {
+  UPDATE_PLAYDATA,
+  UPDATE_USER,
+  UPDATE_GOLD,
+  UPDATE_MULTIPLAY,
+  UPDATE_SINGLEPLAY,
+  UPDATE_TICKET
+} from "../actions/playData/types";
 
 const initialState: PlayData & {loaded: boolean} = {
   multi: [],
   single: [],
   user: {
-    gold: 0,
+    gold: 500,
     id: null,
     isTemp: true,
     name: 'tempOfflineLogin',
-    ticket: 0,
+    ticket: 3,
     googleId: undefined,
     items: "",
   },
@@ -20,26 +30,45 @@ const initialState: PlayData & {loaded: boolean} = {
 const reducer = (state = initialState, action: PlayDataActions) => {
   let newState = {...state};
   if (action.type === UPDATE_PLAYDATA) {
-    const data = action.payload as PlayData;
+    const data = action.payload;
     newState = {
       ...data,
       loaded: true,
     };
   }
+
   if (action.type === UPDATE_USER) {
-    const user = action.payload as SortIoUser;
+    const user = action.payload;
     newState = {
       ...state,
       loaded: true,
       user,
     }
   }
-  
+
+  if (action.type === UPDATE_GOLD) {
+    const newAmount = action.payload;
+    newState.user.gold = newAmount;
+  }
+
+  if (action.type === UPDATE_TICKET) {
+    const newAmount = action.payload;
+    newState.user.ticket = newAmount;
+  }
+
+  if (action.type === UPDATE_SINGLEPLAY) {
+    const newData = action.payload;
+    newState.single = newData;
+  }
+
+  if (action.type === UPDATE_MULTIPLAY) {
+    const newData = action.payload;
+    newState.multi = newData;
+  }
+
   if (state.loaded) {
     setLocalPlayData(newState);
   }
-
-  console.log(newState);
 
   return newState;
 }

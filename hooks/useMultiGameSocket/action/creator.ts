a4930c6c253c7e549793b6fc21d73672105328f9
monthem <lpoeh01@gmail.com>
2020-09-11 23:20:25 +0900

    MISC: moved socket action creator to hooks/userMultiGameSocket/action/

---
+++ hooks/useMultiGameSocket/action/creator.ts
@@ -0,0 +1,101 @@
+import { SocketClientMessageTypes } from "../ClientMessage";
+
+type BasicParam = {
+  userId: number;
+  roomId: number;
+}
+
+type EnterParam = {
+  userId: number
+}
+
+export const enter = (param: EnterParam) => JSON.stringify({
+  type: SocketClientMessageTypes.ENTER,
+  payload: param
+})
+
+type DockParam = {
+  userId: number, roomId: number, stackIndex: number, action: 'DOCK' | 'UNDOCK'
+}
+
+export const dock = (param: DockParam) => JSON.stringify({
+  type: SocketClientMessageTypes.DOCK,
+  payload: param
+})
+
+export const alertLoaded = (param: BasicParam) => JSON.stringify({
+  type: SocketClientMessageTypes.LOADED,
+  payload: param
+})
+
+type UpdateScoreParam = {
+  roomId: number, userId: number, score: number
+}
+
+export const updateScore = (param: UpdateScoreParam) => JSON.stringify({
+  type: SocketClientMessageTypes.UPDATE_SCORE,
+  payload: param
+})
+
+export const alertDisconnect = (param: BasicParam) => JSON.stringify({
+  type: SocketClientMessageTypes.ALERT_DISCONNECT,
+  payload: param
+})
+
+export const success = (param: BasicParam) => JSON.stringify({
+  type: SocketClientMessageTypes.SUCCESS,
+  payload: param,
+})
+
+export const exit = (param: BasicParam) => JSON.stringify({
+  type: SocketClientMessageTypes.EXIT,
+  payload: param,
+})
+
+export const alertReady = (param: BasicParam) => JSON.stringify({
+  type: SocketClientMessageTypes.ALERT_READY,
+  payload: param,
+})
+
+export const alertPrepared = (param: BasicParam) => JSON.stringify({
+  type: SocketClientMessageTypes.ALERT_PREPARED,
+  payload: param,
+})
+
+export const requestRematch = (param: BasicParam) => JSON.stringify({
+  type: SocketClientMessageTypes.REQUEST_REMATCH,
+  payload: param,
+})
+
+export const requestOtherMatch = (param: BasicParam) => JSON.stringify({
+  type: SocketClientMessageTypes.REQUEST_OTHERMATCH,
+  payload: param,
+})
+
+export const cancelRequestRematch = (param: BasicParam) => JSON.stringify({
+  type: SocketClientMessageTypes.CANCEL_REQUEST_REMATCH,
+  payload: param,
+})
+
+export const cancelRequestOtherMatch = (param: BasicParam) => JSON.stringify({
+  type: SocketClientMessageTypes.CANCEL_REQUEST_REMATCH,
+  payload: param,
+})
+
+export const socketClientActions = {
+  enter,
+  dock,
+  alertLoaded,
+  updateScore,
+  alertDisconnect,
+  success,
+  exit,
+  alertReady,
+  alertPrepared,
+  requestRematch,
+  requestOtherMatch,
+  cancelRequestRematch,
+  cancelRequestOtherMatch,
+}
+
+export default socketClientActions;

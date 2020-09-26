f633b8d16f6e5eaa7c111fb969658453ecf0559f
monthem <lpoeh01@gmail.com>
2020-09-26 17:26:39 +0900

    MISC: moved getIcon into /api/icon.tsx

--- components/Profile/Expressions.tsx
+++ components/Profile/Expressions.tsx
@@ -1,55 +1,6 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import {IconProps} from 'react-native-vector-icons/Icon'
-
-const iconSet = {
-  antDesign: require('react-native-vector-icons/AntDesign'),
-  entypo: require('react-native-vector-icons/Entypo'),
-  fontAwesome: require('react-native-vector-icons/FontAwesome'),
-  fontAwesome5: require('react-native-vector-icons/FontAwesome5')
-}
-
-type IconOption = {
-  size?: number;
-  color?: string;
-}
-
-const defaultOption: IconOption = {
-  size: 40,
-  color: "black",
-}
-
-type ContainerProps = {
-  size?: number;
-  color?: string;
-  border?: string;
-  borderWidth?: number;
-}
-
-const IconContainer: React.FC<ContainerProps> = (props) => {
-  const {size = 45, color = "black", border = "none", borderWidth = 2} = props;
-  return (
-    <View style={{
-      backgroundColor: color,
-      borderRadius: 100,
-      width: size,
-      height: size,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderWidth: border === "none" ? 0 : borderWidth,
-      borderColor: border === "none" ? "transparent" : border,
-    }}>
-      {props.children}
-    </View>
-  )
-}
-
-const getIcon = (targetSet: keyof typeof iconSet, iconName: string, option?: IconOption) => {
-  const Icon: React.FC<IconProps> = iconSet[targetSet].default;
-  return (
-    <Icon name={iconName} {...{...defaultOption, ...option}} />
-  )
-}
+import { View } from 'react-native';
+import { IconContainer, getIcon } from '../../api/icon';
 
 const expressions = {
   like: (

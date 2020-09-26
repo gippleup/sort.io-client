8db94663acf1e67344b6e8ab6e3cfaabba7b1631
monthem <lpoeh01@gmail.com>
2020-09-26 16:12:07 +0900

    MODIFY: changed ProfileTester to execute express method every 1second

--- screens/dev/ProfileTester.tsx
+++ screens/dev/ProfileTester.tsx
@@ -1,32 +1,28 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import Profile from '../../components/Profile'
+import expressions from '../../components/Profile/Expressions';
 
 const ProfileTester = () => {
   const profileRef = React.createRef<Profile>();
 
   React.useEffect(() => {
-    profileRef.current?.express(
-      <View
-        style={{
-          width: 40,
-          height: 40,
-          backgroundColor: 'blue',
-          borderRadius: 100,
-        }}
-      >
-      </View>,
-      // <></>,
-      "topRight",
-      50
-    )
+    const interval = setInterval(() => {
+      profileRef.current?.express(
+        expressions.tired,
+        "bottomLeft",
+        50
+      )
+    }, 1000)
+
+    return () => {
+      clearInterval(interval);
+    }
   })
 
   return (
     <View style={{width: 200, height: 200, justifyContent: 'center', alignItems: 'center'}}>
-      <View style={{width: 40, height: 40, borderWidth: 1, borderRadius: 100}}>
-        <Profile ref={profileRef} />
-      </View>
+      <Profile ref={profileRef} chatBubbleSize={60} size={50} />
     </View>
   )
 }

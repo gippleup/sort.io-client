e776c018e50f36f347fb9c618cc0de06d99847ea
monthem <lpoeh01@gmail.com>
2020-09-26 16:10:12 +0900

    ADD: added basic expressions for communication

---
+++ components/Profile/Expressions.tsx
@@ -0,0 +1,184 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import {IconProps} from 'react-native-vector-icons/Icon'
+
+const iconSet = {
+  antDesign: require('react-native-vector-icons/AntDesign'),
+  entypo: require('react-native-vector-icons/Entypo'),
+  fontAwesome: require('react-native-vector-icons/FontAwesome'),
+  fontAwesome5: require('react-native-vector-icons/FontAwesome5')
+}
+
+type IconOption = {
+  size?: number;
+  color?: string;
+}
+
+const defaultOption: IconOption = {
+  size: 40,
+  color: "black",
+}
+
+type ContainerProps = {
+  size?: number;
+  color?: string;
+  border?: string;
+  borderWidth?: number;
+}
+
+const IconContainer: React.FC<ContainerProps> = (props) => {
+  const {size = 45, color = "black", border = "none", borderWidth = 2} = props;
+  return (
+    <View style={{
+      backgroundColor: color,
+      borderRadius: 100,
+      width: size,
+      height: size,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderWidth: border === "none" ? 0 : borderWidth,
+      borderColor: border === "none" ? "transparent" : border,
+    }}>
+      {props.children}
+    </View>
+  )
+}
+
+const getIcon = (targetSet: keyof typeof iconSet, iconName: string, option?: IconOption) => {
+  const Icon: React.FC<IconProps> = iconSet[targetSet].default;
+  return (
+    <Icon name={iconName} {...{...defaultOption, ...option}} />
+  )
+}
+
+const expressions = {
+  like: (
+    <IconContainer color="dodgerblue" border="blue">
+      {getIcon("antDesign", "like1", {size: 25, color: "white"})}
+    </IconContainer>
+  ),
+  dislike: (
+    <IconContainer color="tomato" border="black">
+      {getIcon("antDesign", "dislike1", { size: 25, color: "white" })}
+    </IconContainer>
+  ),
+  trophy: (
+    <IconContainer color="yellow" border="goldenrod">
+      {getIcon("antDesign", "Trophy", { color: "goldenrod", size: 30 })}
+    </IconContainer>
+  ),
+  meh: (
+    <IconContainer size={40} color="yellow">
+      {getIcon("antDesign", "meh", { color: "black", size: 40 })}      
+    </IconContainer>
+  ),
+  smileo: (
+    <IconContainer size={40} color="yellow">
+      {getIcon("antDesign", "smileo", { color: "black", size: 40 })}
+    </IconContainer>
+  ),
+  frowno: (
+    <IconContainer size={40} color="yellow">
+      {getIcon("antDesign", "frowno", { color: "black", size: 40 })}
+    </IconContainer>
+  ),
+  wink: (
+    <IconContainer size={40} color="yellow">
+      {getIcon("entypo", "emoji-flirt", {color: "black"})}
+    </IconContainer>
+  ),
+  smile: (
+    <IconContainer size={40} color="yellow">
+      {getIcon("entypo", "emoji-happy", { color: "black" })}
+    </IconContainer>
+  ),
+  neutral: (
+    <IconContainer size={40} color="yellow">
+      {getIcon("entypo", "emoji-neutral", { color: "black" })}
+    </IconContainer>
+  ),
+  sad: (
+    <IconContainer size={40} color="yellow">
+      {getIcon("entypo", "emoji-sad", { color: "black" })}
+    </IconContainer>
+  ),
+  hand: (
+    <IconContainer color="black" >
+      {getIcon("entypo", "hand", { color: "yellow", size: 30 })}
+    </IconContainer>
+  ),
+  heart: (
+    <IconContainer color="tomato">
+      {getIcon("entypo", "heart", { color: "white", size: 30 })}
+    </IconContainer>
+  ),
+  handPeace: (
+    <IconContainer color="yellow" border="black">
+      {getIcon("fontAwesome", "hand-peace-o", { color: "black", size: 30 })}
+    </IconContainer>
+  ),
+  handShake: (
+    <IconContainer color="green" border="limegreen" size={50}>
+      {getIcon("fontAwesome", "handshake-o", { color: "white", size: 25 })}
+    </IconContainer>
+  ),
+  grinBeamSweat: (
+    <IconContainer color="yellow" size={40}>
+      {getIcon("fontAwesome5", "grin-beam-sweat", { color: "dodgerblue", size: 40 })}
+    </IconContainer>
+  ),
+  grinSquitTears: (
+    <IconContainer color="yellow" size={40}>
+      {getIcon("fontAwesome5", "grin-squint-tears", { color: "dodgerblue", size: 40 })}
+    </IconContainer>
+  ),
+  grinSquint: (
+    <IconContainer color="yellow" size={40}>
+      {getIcon("fontAwesome5", "grin-squint", { color: "dodgerblue", size: 40 })}
+    </IconContainer>
+  ),
+  grinTears: (
+    <IconContainer color="yellow" size={40}>
+      <View style={{position: 'absolute', left: -5, width: 50}}>
+        {getIcon("fontAwesome5", "grin-tears", { color: "dodgerblue", size: 40 })}
+      </View>
+    </IconContainer>
+  ),
+  grinTongueSquint: (
+    <IconContainer color="yellow" size={40}>
+      {getIcon("fontAwesome5", "grin-tongue-squint", { color: "dodgerblue", size: 40 })}
+    </IconContainer>
+  ),
+  grinWink: (
+    <IconContainer color="yellow" size={40}>
+      {getIcon("fontAwesome5", "grin-wink", { color: "dodgerblue", size: 40 })}
+    </IconContainer>
+  ),
+  heartBroken: (
+    <IconContainer color="black" size={40} border="tomato">
+      {getIcon("fontAwesome5", "heart-broken", { color: "tomato", size: 25 })}
+    </IconContainer>
+  ),
+  hourglassHalf: (
+    <IconContainer color="orange" size={45} border="red" borderWidth={5}>
+      {getIcon("fontAwesome5", "hourglass-half", { color: "black", size: 20 })}
+    </IconContainer>
+  ),
+  question: (
+    <IconContainer color="dodgerblue" size={45}>
+      {getIcon("fontAwesome5", "question", { color: "white", size: 20 })}
+    </IconContainer>
+  ),
+  redo: (
+    <IconContainer color="dodgerblue" size={45}>
+      {getIcon("fontAwesome5", "redo", { color: "white", size: 20 })}
+    </IconContainer>
+  ),
+  tired: (
+    <IconContainer color="yellow" size={40}>
+      {getIcon("fontAwesome5", "tired", { color: "black", size: 40 })}
+    </IconContainer>
+  ),
+}
+
+export default expressions;

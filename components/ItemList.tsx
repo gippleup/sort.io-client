f7b11659c66bdb999e7cb2445e25fc37c5c94110
monthem <lpoeh01@gmail.com>
2020-10-05 11:28:26 +0900

    MODIFY: lazy load ItemBox component on ItemList

--- components/ItemList.tsx
+++ components/ItemList.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { Suspense } from 'react'
+import { View, Text, Dimensions, ViewStyle } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler';
 import { CategoryFilter } from '../screens/production/Shop/CatogorySelector';
-import ItemBox, { Item, ItemCategory } from './ItemBox';
+import PatternBackground from './GameScene/PatternBackground';
+import { FlexHorizontal, Space } from './Generic/StyledComponents';
+import GradientBlindScrollView from './GradientBlindScrollView';
+import { Item, ItemCategory } from './ItemList/ItemBox';
+
+const ItemBox = React.lazy(() => import('./ItemList/ItemBox'))
 
 type ItemListProps = {
   data: Item[];
   categoryFilter: CategoryFilter;
+  style?: ViewStyle;
 }
 
 const ItemList: React.FC<ItemListProps> = (props) => {
@@ -16,9 +22,20 @@ const ItemList: React.FC<ItemListProps> = (props) => {
   : data.filter((item) => item.category === categoryFilter)
 
   return (
-    <ScrollView>
-      {selectedItems.map((item, i) => <ItemBox key={i} {...item} />)}
-    </ScrollView>
+    <GradientBlindScrollView
+      blindColor="black"
+      blindHeight={100}
+      blindTransitionHeight={200}
+      style={props.style}>
+      <FlexHorizontal style={{flexWrap: 'wrap'}}>
+        {selectedItems.map((item, i) => (
+          <Suspense key={i} fallback={(<View style={{width: '100%', height: 200, backgroundColor: 'grey', marginBottom: 20}}/>)}>
+            <ItemBox {...item} />
+          </Suspense>
+        ))}
+      </FlexHorizontal>
+      <Space height={100}/>
+    </GradientBlindScrollView>
   )
 }
 

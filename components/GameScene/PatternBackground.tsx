2a4fd9bcbc6e6c5b387fb7682c68b85708d3f74a
monthem <lpoeh01@gmail.com>
2020-10-05 11:25:07 +0900

    MODIFY: simplified PatternBackground Component

--- components/GameScene/PatternBackground.tsx
+++ components/GameScene/PatternBackground.tsx
@@ -3,65 +3,20 @@ import {View, Text, Dimensions, Image, ImageSourcePropType, ViewStyle} from 'rea
 
 type PatternBackgroundProps = {
   source: ImageSourcePropType;
-  width: number;
-  height: number;
-  scale: number;
-  style?: ViewStyle;
 };
 
 const PatternBackground: React.FC<PatternBackgroundProps> = (props) => {
-  const [imageLayout, setImageLayout] = React.useState<null | {
-    width: number;
-    height: number;
-  }>(null);
-
-  const columnCount = imageLayout
-    ? Math.ceil(props.width / (imageLayout?.width * props.scale))
-    : 1;
-
-  const rowCount = imageLayout
-    ? Math.ceil(props.height / (imageLayout?.height * props.scale))
-    : 1;
-
-  const layout = Array(rowCount)
-    .fill(1)
-    .map(() => Array(columnCount).fill(1));
-
   return (
-    <View
-      // eslint-disable-next-line react-native/no-inline-styles
+    <Image
       style={{
+        width: '100%',
+        height: '100%',
         position: 'absolute',
-        width: props.width,
-        height: props.height,
-        overflow: 'hidden',
-        ...props.style,
-      }}>
-      {layout.map((row, i) => (
-        // eslint-disable-next-line react-native/no-inline-styles
-        <View key={'row' + i} style={{flexDirection: 'row'}}>
-          {row.map((column, j) => (
-            <Image
-              onLayout={(e) => {
-                if (!imageLayout) {
-                  setImageLayout(e.nativeEvent.layout);
-                }
-              }}
-              key={'image' + i + j}
-              style={[
-                imageLayout
-                  ? {
-                      width: imageLayout.width * props.scale,
-                      height: imageLayout.height * props.scale,
-                    }
-                  : {},
-              ]}
-              source={props.source}
-            />
-          ))}
-        </View>
-      ))}
-    </View>
+      }}
+      resizeMethod="resize"
+      resizeMode="repeat"
+      source={props.source}
+    />
   );
 };
 

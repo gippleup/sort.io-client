b4029d89b5821c63edd75be24728094e8e213c7a
monthem <lpoeh01@gmail.com>
2020-10-05 19:43:54 +0900

    MISC: fixed type for routes.ts

--- router/routes.ts
+++ router/routes.ts
@@ -1,12 +1,13 @@
+import Developer from './Developer';
 import { devParams, devRoutes } from './DevRoutes';
 import { pdRoutes, pdParams } from './ProductionRoutes';
 import { Routes } from './types';
 
-const routes: Routes<RootStackParamList> = {
+const routes: Routes<Omit<RootStackParamList, "Developer">> = {
   ...devRoutes,
   ...pdRoutes,
 };
 
-export type RootStackParamList = Omit<devParams, "Developer"> & pdParams;
+export type RootStackParamList = devParams & pdParams;
 
 export default routes;

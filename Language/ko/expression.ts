0dbec6f4f312c18ef2192dbe96ef7d9d4f1dc64a
monthem <lpoeh01@gmail.com>
2020-10-07 15:28:59 +0900

    ADD: added description for each skin and expression

--- Language/ko/expression.ts
+++ Language/ko/expression.ts
@@ -3,103 +3,103 @@ import { ExpressionTranslation } from "../translation";
 const expression: ExpressionTranslation = {
   dislike: {
     title: '싫어요',
-    description: ''
-    },
+    description: '상대방이 꼴시리게 군다구요? 상대방의 인성에 문제가 있음을 알려줍시다',
+  },
   frowno: {
     title: '으엑',
-    description: '',
+    description: '울고 싶나요? 울지는 마세요. 찌뿌리는 것까지는 봐드릴게요',
   },
   grinBeamSweat: {
-    title: '',
-    description: '',
+    title: '이건 쫌',
+    description: '압도적으로 이길 때 한 번 눌러주면 상대방이 좋아죽습니다',
   },
   grinSquint: {
-    title: '',
-    description: '',
+    title: '꺄하항',
+    description: '초반에 기세가 좋다 싶으면 한 번 써보세요. 도발당한 상대는 분을 못이기고 실수를 저지를 겁니다',
   },
   grinSquitTears: {
-    title: '',
-    description: '',
+    title: '아고 나 죽어',
+    description: '상대방이 헤맨다 싶을 때마다 눌러주시면 승률이 높아질 수 있습니다',
   },
   grinTears: {
-    title: '',
-    description: '',
+    title: '꺄르륵',
+    description: '상대방보다 먼저 끝낸 후에 눌러보세요. 승률이 높아질 수 있습니다. 도발당한 상대가 재도전을 신청할 수 있기 때문입니다. 꽁승 ㅇㅈ?',
   },
   grinTongueSquint: {
-    title: '',
-    description: '',
+    title: '메렁',
+    description: '상대보다 먼저 끝냈다구요? 이 표현을 쓰신 후에 게임이 끝나자마자 나가보세요. 아.. 도타르시스 오진다..',
   },
   grinWink: {
-    title: '',
-    description: '',
+    title: '윙꾸',
+    description: '한 줄을 채울 때마다 눌러보세요. 상대방의 애간장을 녹일 수 있습니다.',
   },
   hand: {
-    title: '',
-    description: '',
+    title: '안녕!',
+    description: '반갑게 인사를 하고 게임을 시작해봅시다.',
   },
   handPeace: {
-    title: '',
-    description: '',
+    title: '쁘이!',
+    description: '정중하게 자신이 승리했음을 보여줍시다. 하나 둘 셋! 쁘이!',
   },
   handShake: {
-    title: '',
-    description: '',
+    title: '조흔 승부였다!',
+    description: '상대방에게 승부가 아주 마음에 들었음을 정중하게 표현해볼까요?',
   },
   heart: {
-    title: '',
-    description: '',
+    title: '하뚜하뚜',
+    description: '상대방이 마음에 든다구요? 하뚜 한 번 날려줍시다',
   },
   heartBroken: {
-    title: '',
-    description: '',
+    title: '하뚜 바사삭',
+    description: '흥칫뿡',
   },
   hourglassHalf: {
-    title: '',
-    description: '',
+    title: '님님 시간 다 됐음',
+    description: '시간이 얼마 남지 않았을 때 한 번 눌러보세요ㅋㅋ 상대 땀흐르는 소리 여기까지 들리누',
   },
   like: {
-    title: '',
-    description: '',
+    title: '좋아요',
+    description: '꽁승했을 때도, 상대방이 정말 잘한 것 같을 때도 쓸 수 있는 아주 조흔 표현입니다',
   },
   meh: {
-    title: '',
-    description: '',
+    title: '...',
+    description: '상대방이 너무 못한다 싶을 때 한번 써보세요. 자괴감이 들게 만들 수 있습니다.',
   },
   neutral: {
-    title: '',
-    description: '',
+    title: 'ㅡㅡ',
+    description: '떨떠름한 기분을 잘 표현할 수 있습니다',
   },
   question: {
-    title: '',
-    description: '',
+    title: '물음표',
+    description: '의문이 있을 때 쓰면 참 좋겠죠?',
   },
   redo: {
-    title: '',
-    description: '',
+    title: '다시 하자',
+    description: '게임이 끝나기 전에 재도전 의사를 밝히는 데 유용하겠죠?',
   },
   sad: {
-    title: '',
-    description: '',
+    title: 'ㅜㅜ',
+    description: '울지 마세요, 찡그리세요',
   },
   smile: {
-    title: '',
-    description: '',
+    title: ':)',
+    description: '빙그레 웃어봅시다',
   },
   smileo: {
-    title: '',
-    description: '',
+    title: 'ㅎㅎ',
+    description: 'ㅎㅎ',
   },
   tired: {
-    title: '',
-    description: '',
+    title: '아고 죽겠다',
+    description: '간발의 차로 이긴 것 같다구요? 힘들어 죽겠다는 표현을 해보세요',
   },
   trophy: {
-    title: '',
-    description: '',
+    title: 'ㅋ이김',
+    description: '시크하게 자신의 승리를 알려봅시다',
   },
   wink: {
-    title: '',
-    description: '',
+    title: '윙크',
+    description: '^_=',
   },
 };
 
--- Language/ko/skin.ts
+++ Language/ko/skin.ts
@@ -3,107 +3,107 @@ import { SkinTranslation } from "../translation";
 const skin: SkinTranslation = {
   baby: {
     title: '베이비',
-    description: '',
+    description: '똥골똥골한 게 마치 젤리빈같죠? 귀염귀염한 게 쌓는 느낌도 좋습니다.',
   },
   basic: {
     title: '베이직',
-    description: '기본 제공 스킨. 초연한 고수의 느낌을 풍길 수 있다.',
+    description: '기본 제공 스킨입니다. 초연한 고수의 느낌을 풍길 수 있습니다.',
   },
   batcap: {
     title: '배트캡',
-    description: '',
+    description: '박쥐를 닮은 블록입니다. 조금 멋있는 것 같기도 한 것 같기도 한 것 같죠?',
   },
   bearing: {
     title: '베어링',
-    description: '',
+    description: '베이비 스킨보다는 조금 더 묵직한 느낌이면서도 부들부들한 느낌을 가지고 있습니다. 쌓다보면 기분이가 좋아버립니다.',
   },
   bow: {
     title: '보우',
-    description: '',
+    description: '엘레강스 스킨을 뒤집은 형태의 블록입니다. 상승하는 아름다움을 담은 스킨이라고 할까요...★',
   },
   castle: {
     title: '캐슬',
-    description: '',
+    description: '촥촥 쌓는 느낌이 아주 좋습니다. 츄라이 츄라이',
   },
   chubby: {
     title: '통통이',
-    description: '',
+    description: '베이비 스킨과 비슷하지만 쌓는 느낌을 더 잘 주기 위해 윗부분이 볼록 튀어나왔습니다.',
   },
   eggpan: {
     title: '후라이팬',
-    description: '',
+    description: '기분 탓인지 모르겠지만 조금 집중이 더 잘 되는 것 같기도...?',
   },
   elegantbat: {
     title: '라운드 배트캡',
-    description: '',
+    description: '배트캡의 각을 최소화한 블록입니다. 촥촥 쌓는 느낌도 잘 들면서 꽤 예쁘게 빠졌네요',
   },
   elegantline: {
     title: '엘레강스',
-    description: '',
+    description: '각을 싸그리 없앤 아주 우아한 형태입니다. 눈이 편하답니다',
   },
   fat: {
     title: '퉁퉁이',
-    description: '',
+    description: '위로 뽀오올 튀어나온 베이비 스킨이라고나 할까요',
   },
   holder: {
     title: '홀더',
-    description: '',
+    description: '각이 촥촥 잡힌 걸 좋아하신다면 한 번 츄라이 해보세요',
   },
   horizon: {
     title: '호라이즌',
-    description: '',
+    description: '쌓아보세요. 모양이가 아주 예쁩니다.',
   },
   invader: {
     title: '인베이더',
-    description: '',
+    description: '뭔가 모양이 심심하다 싶을 때 써보세요ㅋ',
   },
   kiddo: {
     title: '꼬맹이',
-    description: '',
+    description: '베이비 스킨의 양쪽을 조금 뾰족하게 만든 스킨입니다. 베이비 스킨보다 쌓는 느낌이 조금 더 좋은 것 같네요.',
   },
   lazyboy: {
     title: '늘어진다',
-    description: '',
+    description: '뭔가 부드러운 게 축축 늘어진 듯한 모양을 한 스킨입니다. 얼핏 보면 송편 같기도 하구요. 얼마나 쌓았는지를 잘 모르게 되는 게 단점입니다ㅋ',
   },
   mountain: {
     title: '마운틴',
-    description: '',
+    description: '가시성도 높고 집중도 잘 되는 모양 중 하나입니다. 츄라이 해보세요',
   },
   overweight: {
     title: '과체중',
-    description: '',
+    description: '너무 통통하게 생긴 나머지 분명 2D임에도 불구하고 돌탑을 쌓아올린 듯한 입체감을 느끼게 해주는 스킨입니다.',
   },
   pinblock: {
     title: '핀블록',
-    description: '',
+    description: '정말 깔끔한 블록 중 하나입니다. 정리해두면 아주 예쁘답니다',
   },
   pot: {
     title: '도자기',
-    description: '',
+    description: '색상 구분이 조금 더 쉬워질 수 있습니다. 네, 어디까지나 가능성입니다. 저는 그러더라구요.',
   },
   roundbasic: {
     title: '라운드 베이직',
-    description: '',
+    description: '기본 제공되는 스킨을 부드럽게 깎았습니다.',
   },
   roundupward: {
     title: '거꾸러진 라운드',
-    description: '',
+    description: '기본 제공되는 스킨을 부드럽게 깎은 걸 뒤집었습니다.',
   },
   sleepyworm: {
     title: '졸린 꼬물이',
-    description: '',
+    description: '베이비 스킨을 뒤집었습니다.',
   },
   upward: {
     title: '거꾸러진 베이직',
-    description: '',
+    description: '기본 제공 스킨을 뒤집은 형태의 블록입니다. SO COOL 하죠?',
   },
   volcano: {
     title: '볼케이노',
-    description: '',
+    description: '모양이 굉장히 특이하죠? 이렇게 모양이 특이한 덕분에... 어... 음... 예, 모양이 굉장이 특이하죠?',
   },
   wave: {
     title: '웨이브',
-    description: '',
+    description: '보기도 좋고 쌓기도 좋은 스킨입니다.',
   },
 };
 

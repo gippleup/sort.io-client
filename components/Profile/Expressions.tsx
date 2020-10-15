e4c62bd63e9030d8f6066cb60225cbdabb494270
monthem <lpoeh01@gmail.com>
2020-10-15 23:33:39 +0900

    MODIFY: changed expressions to selectively render in grayscale

--- components/Profile/Expressions.tsx
+++ components/Profile/Expressions.tsx
@@ -1,133 +1,138 @@
+import chroma from 'chroma-js';
 import React from 'react';
 import { View } from 'react-native';
 import { IconContainer, getIcon } from '../../api/icon';
 
+const getGrayscale = (color: string) => chroma(color).set('hsl.s', 0).hex();
+
 const expressions = {
-  like: (
-    <IconContainer color="dodgerblue" border="blue">
-      {getIcon("antDesign", "like1", {size: 25, color: "white"})}
+  like: (isActive: boolean) => {
+    return (
+      <IconContainer color={isActive ? "dodgerblue" : getGrayscale("dodgerblue")} border={isActive ? "blue" : getGrayscale("blue")}>
+        {getIcon("antDesign", "like1", { size: 25, color: isActive ? "white" : getGrayscale("white")})}
+      </IconContainer>
+    )
+  },
+  dislike: (isActive: boolean) =>  (
+    <IconContainer color={isActive ? "tomato" : getGrayscale("tomato")} border={isActive ? "black" : getGrayscale("black")}>
+      {getIcon("antDesign", "dislike1", { size: 25, color: isActive ? "white" : getGrayscale("white") })}
     </IconContainer>
   ),
-  dislike: (
-    <IconContainer color="tomato" border="black">
-      {getIcon("antDesign", "dislike1", { size: 25, color: "white" })}
+  trophy: (isActive: boolean) =>  (
+    <IconContainer color={isActive ? "yellow" : getGrayscale("yellow")} border={isActive ? "goldenrod" : getGrayscale("goldenrod")}>
+      {getIcon("antDesign", "Trophy", { color: isActive ? "goldenrod" : getGrayscale("goldenrod"), size: 30 })}
     </IconContainer>
   ),
-  trophy: (
-    <IconContainer color="yellow" border="goldenrod">
-      {getIcon("antDesign", "Trophy", { color: "goldenrod", size: 30 })}
+  meh: (isActive: boolean) => (
+    <IconContainer size={40} color={isActive ? "yellow" : getGrayscale("yellow")}>
+      {getIcon("antDesign", "meh", { color: isActive ? "black" : getGrayscale("black"), size: 40 })}      
     </IconContainer>
   ),
-  meh: (
-    <IconContainer size={40} color="yellow">
-      {getIcon("antDesign", "meh", { color: "black", size: 40 })}      
+  smileo: (isActive: boolean) => (
+    <IconContainer size={40} color={isActive ? "yellow" : getGrayscale("yellow")}>
+      {getIcon("antDesign", "smileo", { color: isActive ? "black" : getGrayscale("black"), size: 40 })}
     </IconContainer>
   ),
-  smileo: (
-    <IconContainer size={40} color="yellow">
-      {getIcon("antDesign", "smileo", { color: "black", size: 40 })}
+  frowno: (isActive: boolean) => (
+    <IconContainer size={40} color={isActive ? "yellow" : getGrayscale("yellow")}>
+      {getIcon("antDesign", "frowno", { color: isActive ? "black" : getGrayscale("black"), size: 40 })}
     </IconContainer>
   ),
-  frowno: (
-    <IconContainer size={40} color="yellow">
-      {getIcon("antDesign", "frowno", { color: "black", size: 40 })}
+  wink: (isActive: boolean) => (
+    <IconContainer size={50} color={isActive ? "yellow" : getGrayscale("yellow")}>
+      {getIcon("entypo", "emoji-flirt", {color: isActive ? "black" : getGrayscale("black")})}
     </IconContainer>
   ),
-  wink: (
-    <IconContainer size={50} color="yellow">
-      {getIcon("entypo", "emoji-flirt", {color: "black"})}
+  smile: (isActive: boolean) => (
+    <IconContainer size={50} color={isActive ? "yellow" : getGrayscale("yellow")}>
+      {getIcon("entypo", "emoji-happy", { color: isActive ? "black" : getGrayscale("black") })}
     </IconContainer>
   ),
-  smile: (
-    <IconContainer size={50} color="yellow">
-      {getIcon("entypo", "emoji-happy", { color: "black" })}
+  neutral: (isActive: boolean) => (
+    <IconContainer size={50} color={isActive ? "yellow" : getGrayscale("yellow")}>
+      {getIcon("entypo", "emoji-neutral", { color: isActive ? "black" : getGrayscale("black") })}
     </IconContainer>
   ),
-  neutral: (
-    <IconContainer size={50} color="yellow">
-      {getIcon("entypo", "emoji-neutral", { color: "black" })}
+  sad: (isActive: boolean) => (
+    <IconContainer size={50} color={isActive ? "yellow" : getGrayscale("yellow")}>
+      {getIcon("entypo", "emoji-sad", { color: isActive ? "black" : getGrayscale("black") })}
     </IconContainer>
   ),
-  sad: (
-    <IconContainer size={50} color="yellow">
-      {getIcon("entypo", "emoji-sad", { color: "black" })}
+  hand: (isActive: boolean) => (
+    <IconContainer color={isActive ? "black" : getGrayscale("black")} >
+      {getIcon("entypo", "hand", { color: isActive ? "yellow" : getGrayscale("yellow"), size: 30 })}
     </IconContainer>
   ),
-  hand: (
-    <IconContainer color="black" >
-      {getIcon("entypo", "hand", { color: "yellow", size: 30 })}
+  heart: (isActive: boolean) => (
+    <IconContainer color={isActive ? "tomato" : getGrayscale("tomato")}>
+      {getIcon("entypo", "heart", { color: isActive ? "white" : getGrayscale("white"), size: 30 })}
     </IconContainer>
   ),
-  heart: (
-    <IconContainer color="tomato">
-      {getIcon("entypo", "heart", { color: "white", size: 30 })}
+  handPeace: (isActive: boolean) => (
+    <IconContainer color={isActive ? "yellow" : getGrayscale("yellow")} border={isActive ? "black" : getGrayscale("black")}>
+      {getIcon("fontAwesome", "hand-peace-o", { color: isActive ? "black" : getGrayscale("black"), size: 30 })}
     </IconContainer>
   ),
-  handPeace: (
-    <IconContainer color="yellow" border="black">
-      {getIcon("fontAwesome", "hand-peace-o", { color: "black", size: 30 })}
+  handShake: (isActive: boolean) => (
+    <IconContainer color={isActive ? "green" : getGrayscale("green")} border={isActive ? "limegreen" : getGrayscale("limegreen")} size={50}>
+      {getIcon("fontAwesome", "handshake-o", { color: isActive ? "white" : getGrayscale("white"), size: 25 })}
     </IconContainer>
   ),
-  handShake: (
-    <IconContainer color="green" border="limegreen" size={50}>
-      {getIcon("fontAwesome", "handshake-o", { color: "white", size: 25 })}
+  grinBeamSweat: (isActive: boolean) => (
+    <IconContainer color={isActive ? "yellow" : getGrayscale("yellow")} size={50}>
+      {getIcon("fontAwesome5", "grin-beam-sweat", { color: isActive ? "dodgerblue" : getGrayscale("dodgerblue"), size: 40 })}
     </IconContainer>
   ),
-  grinBeamSweat: (
-    <IconContainer color="yellow" size={50}>
-      {getIcon("fontAwesome5", "grin-beam-sweat", { color: "dodgerblue", size: 40 })}
+  grinSquitTears: (isActive: boolean) => (
+    <IconContainer color={isActive ? "yellow" : getGrayscale("yellow")} size={50}>
+      {getIcon("fontAwesome5", "grin-squint-tears", { color: isActive ? "dodgerblue" : getGrayscale("dodgerblue"), size: 40 })}
     </IconContainer>
   ),
-  grinSquitTears: (
-    <IconContainer color="yellow" size={50}>
-      {getIcon("fontAwesome5", "grin-squint-tears", { color: "dodgerblue", size: 40 })}
+  grinSquint: (isActive: boolean) => (
+    <IconContainer color={isActive ? "yellow" : getGrayscale("yellow")} size={50}>
+      {getIcon("fontAwesome5", "grin-squint", { color: isActive ? "dodgerblue" : getGrayscale("dodgerblue"), size: 40 })}
     </IconContainer>
   ),
-  grinSquint: (
-    <IconContainer color="yellow" size={50}>
-      {getIcon("fontAwesome5", "grin-squint", { color: "dodgerblue", size: 40 })}
-    </IconContainer>
-  ),
-  grinTears: (
-    <IconContainer color="yellow" size={50}>
+  grinTears: (isActive: boolean) => (
+    <IconContainer color={isActive ? "yellow" : getGrayscale("yellow")} size={50}>
       <View style={{position: 'absolute', width: 50}}>
-        {getIcon("fontAwesome5", "grin-tears", { color: "dodgerblue", size: 40 })}
+        {getIcon("fontAwesome5", "grin-tears", { color: isActive ? "dodgerblue" : getGrayscale("dodgerblue"), size: 40 })}
       </View>
     </IconContainer>
   ),
-  grinTongueSquint: (
-    <IconContainer color="yellow" size={50}>
-      {getIcon("fontAwesome5", "grin-tongue-squint", { color: "dodgerblue", size: 40 })}
+  grinTongueSquint: (isActive: boolean) => (
+    <IconContainer color={isActive ? "yellow" : getGrayscale("yellow")} size={50}>
+      {getIcon("fontAwesome5", "grin-tongue-squint", { color: isActive ? "dodgerblue" : getGrayscale("dodgerblue"), size: 40 })}
     </IconContainer>
   ),
-  grinWink: (
-    <IconContainer color="yellow" size={50}>
-      {getIcon("fontAwesome5", "grin-wink", { color: "dodgerblue", size: 40 })}
+  grinWink: (isActive: boolean) => (
+    <IconContainer color={isActive ? "yellow" : getGrayscale("yellow")} size={50}>
+      {getIcon("fontAwesome5", "grin-wink", { color: isActive ? "dodgerblue" : getGrayscale("dodgerblue"), size: 40 })}
     </IconContainer>
   ),
-  heartBroken: (
-    <IconContainer color="black" size={40} border="tomato">
-      {getIcon("fontAwesome5", "heart-broken", { color: "tomato", size: 25 })}
+  heartBroken: (isActive: boolean) => (
+    <IconContainer color={isActive ? "black" : getGrayscale("black")} size={40} border={isActive ? "tomato" : getGrayscale("tomato")}>
+      {getIcon("fontAwesome5", "heart-broken", { color: isActive ? "tomato" : getGrayscale("tomato"), size: 25 })}
     </IconContainer>
   ),
-  hourglassHalf: (
-    <IconContainer color="orange" size={45} border="red" borderWidth={5}>
-      {getIcon("fontAwesome5", "hourglass-half", { color: "black", size: 20 })}
+  hourglassHalf: (isActive: boolean) => (
+    <IconContainer color={isActive ? "orange" : getGrayscale("orange")} size={45} border={isActive ? "red" : getGrayscale("red")} borderWidth={5}>
+      {getIcon("fontAwesome5", "hourglass-half", { color: isActive ? "black" : getGrayscale("black"), size: 20 })}
     </IconContainer>
   ),
-  question: (
-    <IconContainer color="dodgerblue" size={45}>
-      {getIcon("fontAwesome5", "question", { color: "white", size: 20 })}
+  question: (isActive: boolean) => (
+    <IconContainer color={isActive ? "dodgerblue" : getGrayscale("dodgerblue")} size={45}>
+      {getIcon("fontAwesome5", "question", { color: isActive ? "white" : getGrayscale("white"), size: 20 })}
     </IconContainer>
   ),
-  redo: (
-    <IconContainer color="dodgerblue" size={45}>
-      {getIcon("fontAwesome5", "redo", { color: "white", size: 20 })}
+  redo: (isActive: boolean) => (
+    <IconContainer color={isActive ? "dodgerblue" : getGrayscale("dodgerblue")} size={45}>
+      {getIcon("fontAwesome5", "redo", { color: isActive ? "white" : getGrayscale("white"), size: 20 })}
     </IconContainer>
   ),
-  tired: (
-    <IconContainer color="yellow" size={50}>
-      {getIcon("fontAwesome5", "tired", { color: "black", size: 40 })}
+  tired: (isActive: boolean) => (
+    <IconContainer color={isActive ? "yellow" : getGrayscale("yellow")} size={50}>
+      {getIcon("fontAwesome5", "tired", { color: isActive ? "black" : getGrayscale("black"), size: 40 })}
     </IconContainer>
   ),
 }

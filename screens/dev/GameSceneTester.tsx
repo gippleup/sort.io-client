fdaf8bcdd8fb64e6a6a8ffb903607df62db5dc99
monthem <lpoeh01@gmail.com>
2020-10-19 14:10:47 +0900

    MISC: miscellaneous changes

--- screens/dev/GameSceneTester.tsx
+++ screens/dev/GameSceneTester.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import GameScene from '../../components/GameScene';
 import {BlockTypes} from '../../components/Block/Types';
 import generateMap from '../../algo/generateMap';
+import { findNodeHandle, GestureResponderEvent, View } from 'react-native';
+import Profile from '../../components/Profile';
+import { useNavigation } from '@react-navigation/native';
+import useGlobal from '../../hooks/useGlobal';
+import expressions from '../../components/Profile/Expressions';
 
 
 const GameSceneTester = () => {
+  const navigation = useNavigation();
+
   const option = {
     blockStackCount: 21,
     colorCount: 17,
@@ -13,15 +20,36 @@ const GameSceneTester = () => {
     stackLengthMin: 2,
     shuffleCount: 100,
   }
+  
+  const global = useGlobal();
+  const playerProfileRef = React.createRef<Profile>();
+  const opponentProfileRef = React.createRef<Profile>();
+
   return (
     <GameScene
       playerSkin="horizon"
       map={generateMap(option).question}
       title={'하드'}
-      timeLimit={60}
+      timeLimit={5}
+      fps={60}
       maxScore={option.maxScore}
-      mode={'single'}
+      mode={'multi'}
+      playerProfile={{ name: 'me', photo: <Profile ref={playerProfileRef} chatBubbleSize={50} iconColor="dodgerblue" /> }}
+      opponentProfile={{ name: 'opponent', photo: <Profile ref={opponentProfileRef} chatBubbleSize={50} iconColor="black" /> }}
       onComplete={() => console.log('끝남')}
+      playerDockEasing="easeInOutSine"
+      playerDockEasingDuraton={100}
+      timerFps={1}
+      timerRoundTo={0}
+      expressions={global.expressions}
+      onPressExpression={(direction) => {
+        const expression = global.expressions[direction];
+        if (expression) {
+          const element = expressions[expression](true);
+          playerProfileRef.current?.express(element, "topLeft", 50);
+          opponentProfileRef.current?.express(element, "bottomLeft", 50);
+        }
+      }}
     />
   );
 };
--- screens/dev/GoogleSignInTester.tsx
+++ screens/dev/GoogleSignInTester.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import GoogleSignin from '../../components/GoogleSignin'
+import CustomGoogleSignin from '../../components/CustomGoogleSignin'
 
 const GoogleSignInTester = () => {
   return (
     <View>
-      <GoogleSignin/>
+      <CustomGoogleSignin/>
     </View>
   )
 }

04f39ac24df98e52089962d09bb78e88fab2bc5c
monthem <lpoeh01@gmail.com>
2020-10-22 15:03:50 +0900

    ADD: added utils for SelectStageRankPopup

---
+++ screens/production/SelectStage/utils.ts
@@ -0,0 +1,30 @@
+type DayDescriptor = {
+  [index in 0 | 1 | 2 | 3 | 4 | 5 | 6]: string;
+}
+
+export const prettyTime = (time: string, format?: string, dayDescriptor?: DayDescriptor) => {
+  const date = new Date(time);
+  const split = {
+    year: String(date.getFullYear()),
+    month: String(date.getMonth()),
+    day: date.getDay(),
+    date: String(date.getDate()),
+    hours: String(date.getHours()),
+    minutes: String(date.getMinutes()),
+    seconds: String(date.getSeconds()),
+  };
+
+  if (format) {
+    const formatted = format
+      .replace("$YYYY", split.year)
+      .replace("$MM", split.month)
+      .replace("$DD", split.date)
+      .replace("$dd", dayDescriptor ? dayDescriptor[split.day as 0 | 1 | 2 | 3 | 4 | 5 | 6] : String(split.day))
+      .replace("$hh", split.hours)
+      .replace("$mm", split.minutes)
+      .replace("$ss", split.seconds)
+    return formatted;
+  }
+
+  return date.toDateString();
+}

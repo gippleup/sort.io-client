a8a6b7b67ecc986a9a4894bd045194ac32691b00
monthem <lpoeh01@gmail.com>
2020-10-23 23:16:57 +0900

    ADD: added admob api and corresponding settings

---
+++ src/api/admob.ts
@@ -0,0 +1,45 @@
+import admob, { InterstitialAd, MaxAdContentRating, RewardedAd, TestIds } from '@react-native-firebase/admob';
+import BuildConfig from 'react-native-config';
+
+const {BUILD_ENV} = BuildConfig;
+
+export const adUnitId = {
+  banner: BUILD_ENV === "RELEASE" ? "ca-app-pub-1726878540330527/3652806997" : TestIds.BANNER,
+  interstitial: BUILD_ENV === "RELEASE" ? "ca-app-pub-1726878540330527/7400480312" : TestIds.INTERSTITIAL,
+  rewarded: BUILD_ENV === "RELEASE" ? "ca-app-pub-1726878540330527/1581355031" : TestIds.REWARDED,
+}
+
+admob()
+  .setRequestConfiguration({
+    maxAdContentRating: MaxAdContentRating.PG,
+    tagForChildDirectedTreatment: true,
+    tagForUnderAgeOfConsent: true,
+  })
+  .then(() => {
+    console.log('configred successfully')
+  })
+
+
+export const getInterstial = () => {
+  const interstial = InterstitialAd.createForAdRequest(adUnitId.interstitial);
+  // const unsubscribe = interstial.onAdEvent((type) => {
+  //   if (type === "loaded") {
+  //     interstial.show();
+  //     unsubscribe();
+  //   }
+  // })
+  // interstial.load();
+  return interstial;
+}
+
+export const getRewarded = () => {
+  const rewarded = RewardedAd.createForAdRequest(adUnitId.rewarded);
+  // const unsubscribe = rewarded.onAdEvent((type) => {
+  //   if (type === "rewarded_loaded") {
+  //     rewarded.show();
+  //     unsubscribe();
+  //   }
+  // })
+  // rewarded.load();
+  return rewarded;
+}

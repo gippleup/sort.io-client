88680bcc73216c7c9aaf5f65c53a5453156b5d04
monthem <lpoeh01@gmail.com>
2020-11-02 18:47:40 +0900

    MISC: updated firebase google-services.json

--- src/api/googleOAuth.ts
+++ src/api/googleOAuth.ts
@@ -1,8 +1,9 @@
 import { GoogleSignin, statusCodes, User } from "@react-native-community/google-signin";
 
 GoogleSignin.configure({
-  webClientId: '124695122817-p6ltn0adb2n1frl1r5a4j6g4vnqqkc47.apps.googleusercontent.com',
+  webClientId: '771946967067-6p9g18d2t1iq3332mp513ei2bhin1gsu.apps.googleusercontent.com',
   forceCodeForRefreshToken: true,
+  offlineAccess: true,
 })
 
 export const googleSignIn = async (): Promise<User | undefined> => {
--- src/components/CustomGoogleSignin.tsx
+++ src/components/CustomGoogleSignin.tsx
@@ -28,9 +28,11 @@ const PopupButton: typeof View | React.FC<{tomato?: boolean}> = styled(View)<{to
 `;
 
 GoogleSigninModule.configure({
-  webClientId: '124695122817-p6ltn0adb2n1frl1r5a4j6g4vnqqkc47.apps.googleusercontent.com',
+  webClientId: '771946967067-6p9g18d2t1iq3332mp513ei2bhin1gsu.apps.googleusercontent.com',
   forceCodeForRefreshToken: true,
+  offlineAccess: true,
 })
+
 
 type CustomGoogleSignInProps = {
   onSignIn?: (user: User) => any;

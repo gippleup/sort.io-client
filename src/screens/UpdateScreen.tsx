2a977b6d98de162b392cfccf307f03d8564f4b31
monthem <lpoeh01@gmail.com>
2020-11-14 22:56:02 +0900

    MODIFY: changed blockGoBack base from BackHandler to navigation.addListener

--- src/screens/UpdateScreen.tsx
+++ src/screens/UpdateScreen.tsx
@@ -5,6 +5,7 @@ import Updater from '../components/Updater'
 import BuildConfig from 'react-native-config';
 import { RootStackParamList } from '../router/routes';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { modifyToTargetRoutes } from '../api/navigation';
 
 const {BUILD_ENV} = BuildConfig;
 
@@ -15,36 +16,30 @@ type UpdateScreenProps = {
 
 const UpdateScreen = (props: UpdateScreenProps) => {
   const navigation = props.navigation;
-  const navigateTo = (routeName: keyof RootStackParamList) => {
-    navigation.dispatch((state) => {
-      return CommonActions.reset({
-        ...state,
-        routes: [{
-          key: routeName + Date.now(),
-          name: routeName,
-        }],
-        index: 0,
-      })
-    })
-  }
+
+  const unsubscribeBeforeRemove = navigation.addListener("beforeRemove", (e) => {
+    if (e.data.action.type === "GO_BACK") {
+      e.preventDefault();
+    }
+  })
 
   const onFinish = () => {
+    unsubscribeBeforeRemove();
     if (BUILD_ENV === "RELEASE") {
-      navigateTo("Main");
+      modifyToTargetRoutes(navigation, [
+        {name: "LoadingScreen"},
+        {name: "Main"},
+      ])
     } else {
-      navigateTo("Developer");
+      modifyToTargetRoutes(navigation, [
+        {name: "Developer"},
+      ]);
     }
   }
 
-  const blockBack = () => {
-    return true;
-  }
-
-  BackHandler.addEventListener("hardwareBackPress", blockBack);
-
   React.useEffect(() => {
     return () => {
-      BackHandler.removeEventListener("hardwareBackPress", blockBack);
+      unsubscribeBeforeRemove();
     }
   })
 
